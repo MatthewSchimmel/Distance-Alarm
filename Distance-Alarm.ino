06d38c23a23875377d0085c6d8e52936adaba8e2
mattshim <matt.schimmel@tutanota.com>
2023-12-06 23:37:34 -0700

    2nd working ver. w/better thresh. buttons

--- Distance-Alarm.ino
+++ Distance-Alarm.ino
@@ -1,5 +1,6 @@
 /*  Written by Matthew Ryan Schimmel in December of 2023
-  matthewschimmel.site, 
+  matthewschimmel.site
+  Github: github.com/MatthewSchimmel/Distance-Alarm
 */
 #include <Arduino_GFX_Library.h>
 #define TFT_SCK    3
@@ -35,9 +36,21 @@ Arduino_ILI9341 display = Arduino_ILI9341(&bus, TFT_RESET);
 // Set these to your desired credentials.
 const char *ssid = "Nittany";
 const char *password = "";
-//remember: the website to visit is: http://192.168.4.1
+//the website to visit is: http://192.168.4.1
 WiFiServer server(80);
-//------------------//
+String html ="<!DOCTYPE html> \
+<html> \
+<body> \
+<center><h1>ESP32 Soft access point</h1></center> \
+<center><h2>Web Server</h2></center> \
+<form> \
+<button name=\"NOISE\" button style=\"color:green\" value=\"ON\" type=\"submit\">NOISE ON</button> \
+<button name=\"NOISE\" button style=\"color=red\" value=\"OFF\" type=\"submit\">NOISE OFF</button><br><br> \
+<button name=\"THRESHOLD\" button style=\"color:green\" value=\"UP\" type=\"submit\">THRESHOLD UP</button> \
+<button name=\"THRESHOLD\" button style=\"color:red\" value=\"DOWN\" type=\"submit\">THRESHOLD DOWN</button> \
+</form> \
+</body> \
+</html>";
 //touchPin: lecture 21
 //screen: lecture 21
 long duration;  // variable for the duration of sound wave travel
@@ -87,110 +100,6 @@ void clearScreen(){
   display.fillScreen(BLACK);
   display.setCursor(0, 1);
 }
-
-void setupDisplay(){
-  display.begin();
-  display.fillScreen(BLACK);
-  display.setCursor(0, 1);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.println("Display Initialized");
-}
-
-void setupServer(){
-  Serial.begin(115200);
-  displayPrint("Setting A.P...");
-  // You can remove the password parameter if you want the AP to be open.
-  // a valid password must have more than 7 characters
-  if (!WiFi.softAP(ssid, password)) {
-    log_e("Soft AP creation failed.");
-    while (1)
-      ;
-  }
-  IPAddress myIP = WiFi.softAPIP();
-  //This part cannot be obfuscated through displayPrint, so don't try.
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
-  Serial.println("Server started");
-  display.print("AP IP address: ");
-  display.println(myIP);
-  display.println("Server started");
-  server.begin();
-}
-
-void loop() {
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds. Given code.
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;  // Calculating the distance
-  //checkThreshold();
-  //Prints the distance, all on one
-  clearScreen();
-  illuminateDistance(distance);
-  triggerAlarm((distance <= threshold) || buttonIsPressed());
-
-  //-- WiFi management --//
-  WiFiClient client = server.available();  // listen for incoming clients
-
-  if (client) {                     // if you get a client,
-    Serial.println("New Client.");  // print a message out the serial port
-    display.println("New Client.");  // print a message out the display
-    String currentLine = "";        // make a String to hold incoming data from the client
-    while (client.connected()) {    // loop while the client's connected
-      if (client.available()) {     // if there's bytes to read from the client,
-        char c = client.read();     // read a byte, then
-        Serial.write(c);            // print it out the serial monitor
-        if (c == '\n') {            // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("<span style=font-size:6em>Click <a href=\"/H\">here</a> to turn ON the buzzer.<br>");
-            client.print("<span style=font-size:6em>Click <a href=\"/L\">here</a> to turn OFF the buzzer.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {  // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          noise = true;
-        }
-        if (currentLine.endsWith("GET /L")) {
-          noise = false;
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
-    display.println("Client Disconnected.");
-  }
-  delay(250);
-}
-
 void displayPrint(String newline){
   shiftLines();
   output[0] = newline;
@@ -209,22 +118,38 @@ void printOutput(){
   }
   Serial.println(output[0]);
 }
+int getNumOfLights(){
+  return getNumOfLights(0.3);
+}
 
-void checkThreshold() {
-  int UpButtonVal = digitalRead(UpButton);  //param = button GP pin
-  int DownButtonVal = digitalRead(DownButton);  //param = button GP pin
-  if (UpButtonVal == LOW && threshold > 10) {                //if button is pressed
-    threshold -= 10;
-    //displayPrint("Threshold decreased to " + threshold + "cm")
-    flash(threshold / 10);
+int getNumOfLights(double ratio){// algorithm determines how many thresholds away the user is
+double interval = threshold * ratio;
+  if(buttonIsPressed()){
+    return 10;
+  } else {
+    return (10 - ((distance - threshold) / interval)); //the closer to threshold, the more lights
   }
-  if (DownButtonVal == LOW) {  //if button is pressed
+}
+void changeThreshold(bool direction){
+  if (direction == false) {// if direction is downward
+    if (threshold <= 10) {
+      threshold -= 10;
+      String output = "Threshold down to ";
+      output += threshold;
+      output += "cm";
+      displayPrint(output);
+      flash(threshold / 10);
+    }
+  }
+  if (direction == true) {// if direction is upward
     threshold += 10;
-    //displayPrint("Threshold increased to " + threshold + "cm")
+    String output = "Threshold up to ";
+    output += threshold;
+    output += "cm";
+    displayPrint(output);
     flash(threshold / 10);
   }
 }
-
 void flash(int iterations){
   for (int i = 0; i < iterations ; i++) {
     digitalWrite(BUILTIN_LED, HIGH);
@@ -233,17 +158,6 @@ void flash(int iterations){
     delay(100);
   }
 }
-
-bool buttonIsPressed() {
-  int buttonVal = digitalRead(alarmButton);  //param = button GP pin
-  if (buttonVal == LOW) {                    //if button is pressed
-    return true;
-  } else {
-    return false;
-  }
-}
-
-
 
 void triggerAlarm() {
   triggerAlarm(false);
@@ -296,11 +210,58 @@ void alarmLights(bool trigger) {
   }
 }
 
-void illuminateDistance(){
-   illuminateDistance(.3);
+void clearTheLights(){
+  digitalWrite(led1, LOW);  //led off
+  digitalWrite(led2, LOW);  //led off
+  digitalWrite(led3, LOW);  //led off
+  digitalWrite(led4, LOW);  //led off
+  digitalWrite(led5, LOW);  //led off
+  digitalWrite(led6, LOW);  //led off
+  digitalWrite(led7, LOW);  //led off
+  digitalWrite(led8, LOW);  //led off
+  digitalWrite(led9, LOW);  //led off
+  digitalWrite(led10, LOW);  //led off
 }
-void illuminateDistance(double ratio){
-  int numOfLights = getNumOfLights(ratio);
+
+void setupDisplay(){
+  display.begin();
+  display.fillScreen(BLACK);
+  display.setCursor(0, 1);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.println("Display Initialized");
+}
+void setupServer(){
+  Serial.begin(115200);
+  displayPrint("Setting A.P...");
+  // You can remove the password parameter if you want the AP to be open.
+  // a valid password must have more than 7 characters
+  if (!WiFi.softAP(ssid, password)) {
+    log_e("Soft AP creation failed.");
+    while (1)
+      ;
+  }
+  IPAddress myIP = WiFi.softAPIP();
+  //This part cannot be obfuscated through displayPrint, so don't try.
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  Serial.println("Server started");
+  display.print("AP IP address: ");
+  display.println(myIP);
+  display.println("Server started");
+  server.begin();
+}
+void illuminateDistance(int distance){
+   illuminateDistance(distance, .3);
+}
+void illuminateDistance(int distance, double ratio){
+  double interval = threshold * ratio;
+  int numOfLights;
+  if(buttonIsPressed()){
+    numOfLights = 10;
+  } else {
+    numOfLights = 10 - ((distance - threshold) / interval); //the closer to threshold, the more lights
+  }
   clearTheLights();
   if(numOfLights > 0){
     digitalWrite(led1, HIGH);  //led on
@@ -333,26 +294,44 @@ void illuminateDistance(double ratio){
     digitalWrite(led10, HIGH);  //led on
   }
 }
-int getNumOfLights(){
-  return getNumOfLights(0.3);
-}
-int getNumOfLights(double ratio){//algorithm determines how many thresholds away the user is
-double interval = threshold * ratio;
-  if(buttonIsPressed()){
-    return 10;
+bool buttonIsPressed() {
+  int buttonVal = digitalRead(alarmButton);  //param = button GP pin
+  if (buttonVal == LOW) {                    //if button is pressed
+    return true;
   } else {
-    return 10 - ((distance - threshold) / interval); //the closer to threshold, the more lights
+    return false;
   }
 }
-void clearTheLights(){
-  digitalWrite(led1, LOW);  //led off
-  digitalWrite(led2, LOW);  //led off
-  digitalWrite(led3, LOW);  //led off
-  digitalWrite(led4, LOW);  //led off
-  digitalWrite(led5, LOW);  //led off
-  digitalWrite(led6, LOW);  //led off
-  digitalWrite(led7, LOW);  //led off
-  digitalWrite(led8, LOW);  //led off
-  digitalWrite(led9, LOW);  //led off
-  digitalWrite(led10, LOW);  //led off
+
+void loop() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds. Given code.
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;  // Calculating the distance
+  clearScreen();
+  illuminateDistance(distance);
+  triggerAlarm((distance <= threshold) || buttonIsPressed());
+
+  //-- WiFi management --//
+  WiFiClient client = server.available();  // listen for incoming clients
+
+  if (client) {// if you get a client,
+    String request = client.readStringUntil('\r');
+    if(request.indexOf("NOISE=ON") != -1) noise = true;
+    if(request.indexOf("NOISE=OFF") != -1) noise = false;
+    if(request.indexOf("THRESHOLD=UP") != -1) changeThreshold(true);
+    if(request.indexOf("THRESHOLD=DOWN") != -1) changeThreshold(false);            
+    client.print(html);
+    client.stop();
+    displayPrint("Client Disconnected.");
+  }
+  delay(250);
 }
